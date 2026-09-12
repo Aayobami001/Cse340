7a110102d55f19af4c215cddd2bb0f45f4be0671
edemfrancis <efrancis.edem@gmail.com>
2026-09-12 22:54:03 +0100

    Week 1, Assignment done

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const path = require("path");
+
+// Define the application environment
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || "production";
+// Define the port number the server will listen on
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+/**
+  Configure Express middleware
+*/
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Routes
+ */
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+
+app.get('/organizations', async (req, res) => {
+    const title = 'Our Partner Organizations';
+    res.render('organizations', { title });
+});
+
+app.get('/projects', async (req, res) => {
+    const title = 'Service Projects';
+    res.render('projects', { title });
+});
+
+app.get('/categories', async (req, res) => {
+    const title = 'Service Categories';
+    res.render('categories', { title });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
