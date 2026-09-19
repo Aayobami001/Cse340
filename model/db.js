6d11b20e0436f97186b3421614e5d09d64aa6322
aibrahim.akinbiyi@gmail.com <efrancis.edem@gmail.com>
2026-09-19 06:08:53 +0100

    add Unauthorized to ssl

--- model/db.js
+++ model/db.js
@@ -16,8 +16,9 @@ import 'dotenv/config';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true,
-    rejectUnauthorized: false
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 /**
