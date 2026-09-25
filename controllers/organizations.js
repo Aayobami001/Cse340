e43e5328469c3b7a7f8f153df8bb0a11c9dba04d
aibrahim.akinbiyi@gmail.com <efrancis.edem@gmail.com>
2026-09-25 15:27:26 +0100

    Week 03 assignment

--- controllers/organizations.js
+++ controllers/organizations.js
@@ -1,4 +1,5 @@
-import { getAllOrganizations } from "../model/organizations.js";
+import { getAllOrganizations, getOrganizationDetails } from "../model/organizations.js";
+import { getProjectsByOrganizationId } from "../model/projects.js";
 
 const showOrganizationsPage = async (req, res) => {
   // added the getAllOrganizations function to retrieve the organizations from the database wk 02
@@ -7,4 +8,13 @@ const showOrganizationsPage = async (req, res) => {
   res.render("organizations", { title, organizations });
 };
 
-export { showOrganizationsPage };
+const showOrganizationDetailsPage = async (req, res) => {
+    const organizationId = req.params.id;
+    const organizationDetails = await getOrganizationDetails(organizationId);
+    const projects = await getProjectsByOrganizationId(organizationId);
+    const title = 'Organization Details';
+
+    res.render('organization', {title, organizationDetails, projects});
+};
+
+export { showOrganizationsPage, showOrganizationDetailsPage };
--- routes.js
+++ routes.js
@@ -1,18 +1,22 @@
-import express from 'express';
+import express from "express";
 
-import { showHomePage } from './controllers/index.js';
-import { showOrganizationsPage } from './controllers/organizations.js';
-import { showProjectsPage } from './controllers/projects.js';
-import { showCategoriesPage } from './controllers/categories.js';
-import { showTestErrorPage } from './controllers/errors.js';
+import { showHomePage } from "./controllers/index.js";
+import { showOrganizationsPage } from "./controllers/organizations.js";
+import { showProjectsPage } from "./controllers/projects.js";
+import { showCategoriesPage } from "./controllers/categories.js";
+import { showTestErrorPage } from "./controllers/errors.js";
+import { showOrganizationDetailsPage } from './controllers/organizations.js';
+
 const router = express.Router();
 
-router.get('/', showHomePage);
-router.get('/organizations', showOrganizationsPage);
-router.get('/projects', showProjectsPage);
-router.get('/categories', showCategoriesPage);
+router.get("/", showHomePage);
+router.get("/organizations", showOrganizationsPage);
+router.get("/projects", showProjectsPage);
+router.get("/categories", showCategoriesPage);
+// Route for organization details page
+router.get("/organization/:id", showOrganizationDetailsPage);
 
 // error-handling routes
-router.get('/test-error', showTestErrorPage);
+router.get("/test-error", showTestErrorPage);
 
 export default router;
